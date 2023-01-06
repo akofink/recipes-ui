c4b2c6c824543920197ee35d59ef600a17718c7d
Andrew Kofink <ajkofink@gmail.com>
2023-01-06 13:42:22 -0500

    wip list recipes
    Signed-off-by: Andrew Kofink <ajkofink@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,8 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 import './App.scss';
-import { Recipe } from './layouts/recipe';
-
-export const BASE_URL = 'https://raw.githubusercontent.com/akofink/recipes-md/main'
+import { Recipes } from './layouts/recipes';
 
 const App = () => {
-  const [markdown, setMarkdown] = useState("");
-  const { filename } = useParams();
-  const url = `${BASE_URL}/${filename}.md`;
-
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.text())
-      .then((text) => setMarkdown(text));
-  }, [url]);
-
-  return <Recipe markdown={markdown} />;
+  return <Recipes />;
 };
 
 export default App;
