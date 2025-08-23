1fd1cfb7fbebe487718ec35eb2c24a9d84cbbe15
Andrew Kofink <ajkofink@gmail.com>
2025-08-23 16:19:08 -0400

    ui: enable fullscreen modal on image click in recipe view (dismiss on click or close)

--- src/layouts/recipe.tsx
+++ src/layouts/recipe.tsx
@@ -1,8 +1,8 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import ReactMarkdown from 'react-markdown'
 import { useParams } from 'react-router-dom';
 import Navigation from './navigation';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Modal } from 'react-bootstrap';
 import { RECIPESMD_RAW } from '../constants';
 // Note: generated at build-time. During development, run `yarn generate` first.
 import recipesData from '../generated/recipes.json';
@@ -18,6 +18,9 @@ export const Recipe = () => {
   ), [filename])
   const markdown = recipe?.markdown || ''
   const imageNames: string[] = (recipe?.imageNames || []) as string[]
+  const [activeImage, setActiveImage] = useState<string | null>(null);
+  const handleOpen = (img: string) => setActiveImage(img);
+  const handleClose = () => setActiveImage(null);
 
   return (
     <Navigation>
@@ -30,7 +33,8 @@ export const Recipe = () => {
                 <img
                   src={`${RECIPESMD_RAW}/images/${name}/${img}`}
                   alt={`${name} ${img}`}
-                  style={{ width: '100%', height: 200, objectFit: 'cover', borderRadius: 6 }}
+                  style={{ width: '100%', height: 200, objectFit: 'cover', borderRadius: 6, cursor: 'pointer' }}
+                  onClick={() => handleOpen(img)}
                 />
               </Col>
             ))}
@@ -41,6 +45,19 @@ export const Recipe = () => {
         {markdown}
       </ReactMarkdown>
       <a href={`${EDIT_BASE_URL}/${filename}`} target='_blank'>Edit</a>
+
+      <Modal show={!!activeImage} onHide={handleClose} centered size="lg">
+        <Modal.Body style={{ padding: 0, background: '#000' }}>
+          {activeImage && (
+            <img
+              src={`${RECIPESMD_RAW}/images/${name}/${activeImage}`}
+              alt={`${name} ${activeImage}`}
+              style={{ width: '100%', height: 'auto', display: 'block' }}
+              onClick={handleClose}
+            />
+          )}
+        </Modal.Body>
+      </Modal>
     </Navigation>
   )
 }
